2aae65427e5c8c26a37ee26f580b8defc87fee67
lizqwerscott <lizqwerscott@163.com>
2025-03-03 10:52:17 +0800

    feat: add macos support

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-cargo build --release
+cargo build --release &&
 
-ln -sv ./target/release/libzeta_dyn.so ./zeta-dyn.so
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    rm ./zeta-dyn.dylib
+    ln -sv ./target/release/libzeta_dyn.dylib ./zeta-dyn.dylib
+else
+    rm ./zeta-dyn.so
+    ln -sv ./target/release/libzeta_dyn.so ./zeta-dyn.so
+fi
